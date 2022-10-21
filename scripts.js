93969e116d17fbc2ccafbade38d4b67ccf1e90a1
Blake Creasser <blakecreasser@aggienetwork.com>
2022-10-21 00:10:39 -0500

    add basic dark and light mode functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
+const body = document.querySelector(".body")
+const header = document.querySelector('.header')
+const footer = document.querySelector('.footer')
+const aboutMe = document.querySelector('.aboutMe')
+const lang = document.querySelector('.lang')
+const project = document.querySelector('.projects')
+const darkBtn = document.querySelector('#dark')
+const lightBtn = document.querySelector('#light')
 
+function darkMode() {
+    document.body.style.backgroundColor = '#3f3f40'
+    header.style.backgroundColor = '#3c3b58'
+    footer.style.backgroundColor = '#3c3b58'
+    aboutMe.style.border = 'solid 5px #f6f8ff'
+    lang.style.border = 'solid 5px #f6f8ff'
+    project.style.border = 'solid 5px #f6f8ff'
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = '#f6f8ff'
+    header.style.backgroundColor = '#acabc5'
+    footer.style.backgroundColor = '#acabc5'
+    aboutMe.style.border = 'solid 5px black'
+    lang.style.border = 'solid 5px black'
+    project.style.border = 'solid 5px black'
+}
+
+darkBtn.addEventListener('click', darkMode)
+lightBtn.addEventListener('click', lightMode)
